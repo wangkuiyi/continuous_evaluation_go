49b7859e0ea3e00b4ab935b78dd5b532846d1d02
Yi Wang <yiwang@linkedin.com>
2014-10-27 09:15:57 -0700

    Add SendKPI API

--- unit_evaluation_example_test.go
+++ unit_evaluation_example_test.go
@@ -2,16 +2,21 @@ package continuous_evaluation_go
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"testing"
 )
 
-func TestExampleUnitEvaluation(t *testing.T) {
+func SendKPI(kpi, value string) {
 	if ctl := os.Getenv("CONTEVAL_CONTROLLER"); len(ctl) > 0 {
-		http.Get(fmt.Sprintf("%s?commit=%s&kpi=AnExampleKPI&value=10",
-			ctl, os.Getenv("CONTEVAL_COMMIT")))
+		http.Get(fmt.Sprintf("%s?commit=%s&kpi=%s&value=%s",
+			ctl, os.Getenv("CONTEVAL_COMMIT"), kpi, value))
 	} else {
-		fmt.Println("Not started by Calla; skip TestExampleUnitEvaluation")
+		log.Printf("Not started by Calla; no sending %s\n", kpi)
 	}
+}
+
+func TestExampleUnitEvaluation(t *testing.T) {
+	SendKPI("AnExampleKPI", "10")
 }
